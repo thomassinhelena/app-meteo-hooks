05dc37fa19c3a6f33e9a408d7247c5008bbf9b10
Hélèna Thomassin <thomassin.helena@gmail.com>
2020-01-19 18:33:48 +0100

    add a list

--- src/Components/Weather.js
+++ src/Components/Weather.js
@@ -10,6 +10,7 @@ const [temp, setTemp] = useState(0);
 const [humidity, setHumidity] = useState(0);
 const [windSpeed, setWindSpeed] = useState(0);
 const [windDegree, setWindDegree] = useState(0);
+const [list, setList] = useState([]);
 
 
 useEffect(() => {
@@ -17,25 +18,32 @@ useEffect(() => {
     axios.get(`https:api.openweathermap.org/data/2.5/forecast?lat=${position.coords.latitude}&lon=${position.coords.longitude}&cnt=${7}&units=metric&appid=${apiKey}`)
     .then((response) => {
       console.log(response);
-      setName(response.data.name)
+      setName(response.data.city.name)
       setIcon(response.data.list[0].weather[0].icon);
       setIconDescrib(response.data.list[0].weather[0].description);
       setTemp(response.data.list[0].main.temp);
       setHumidity(response.data.list[0].main.humidity);
       setWindSpeed(response.data.list[0].wind.speed);
       setWindDegree(response.data.list[0].wind.deg);
+      setList(response.data.list);
     })
   });
 }, []);
 
 return (
   <Fragment>
-    <h2>{name}</h2>
-    <img src={`https://openweathermap.org/img/wn/10n@2x.png`} alt={iconDescrib} />
-    <p>{temp}°C</p>
-    <p>{windSpeed*3.6}Km/h</p>
-    <p>{humidity}%</p>
-    <img src={`${process.env.PUBLIC_URL}/Assets/Images/map-arrow-png-1.png`}width="100px" style={{transform: `rotate(${windDegree}deg)`}}/>
+    {list.map((item, index) => {
+      return(
+        <div key={index}>
+          <h2>{name}</h2>
+            <img src={`https://openweathermap.org/img/wn/10n@2x.png`} alt={iconDescrib} />
+            <p>{item.wind.speed*3.6}Km/h </p>
+            <p>{item.main.temp}°C </p>
+            <p>{item.main.humidity}% </p>
+            <img src={`${process.env.PUBLIC_URL}/Assets/Images/map-arrow-png-1.png`}width="100px" style={{transform: `rotate(${item.wind.degree}deg)`}} />
+        </div>
+      )
+    })}
   </Fragment>
 )
 }
